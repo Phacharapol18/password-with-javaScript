bc6ee2e865763099b7529fa4e7cfe86d6da9b613
Phacharapol Phukana <phacharapol18@gmail.com>
2022-03-31 02:23:54 -0700

    redo js

--- script.js
+++ script.js
@@ -1,28 +1,44 @@
 function generatePassword(){
   //TODO: your code goes here
-  alert('please! choose password length of at least 8 characters and no more than 128 characters');
-  alert('please! confirm whether or not to include lowercase, uppercase, numeric, and/or special characters');
-  alert('Here is your password, if not satisfy, please click generate password again');
+  let userInput = prompt('please! input minimum password length of at least 8 characters and no more than 128 characters');
+  let userCon = confirm(`Is this your final length: ${userInput}.`)
+  let convert = + userInput;
+   
+ if (userCon){
+    alert('Here is your password, if not satisfy, please click generate password again');
+   } else {
+     return;
+   }
   
-  let randomLetter = ['zlkearg@$#@$taknvoah845r92rjfdkgjh%24'];
+  let randomLetter = ['z','l','k','e','a','r','g','@','#','@','$','t','a','k','n','v','o','a','h','r','r','j','f','d','g','j','h','%'];
   
-  let randomNumber = ['23874175897'];
+  let randomNumber = [2,3,8,4,1,7,5,8,9,6];
   
-  let randomSpecial =['hgjkhgjhgiu&%@'];
-  
-  let randomP = randomLetter.concat(randomNumber, randomSpecial);
-  
-  let randomJ = randomP.join('');
- 
-  let randomText = randomJ.substring(Math.floor(Math.random() * randomJ.length),Math.floor(Math.random() * randomJ.length) + 4);
-  
-  let randomText2 = randomJ.substring(Math.floor(Math.random() * randomJ.length),Math.floor(Math.random() * randomJ.length) + 4);
-  
-  let randomT = randomText.concat(randomText2.toUpperCase().trim());
-  
-  return randomT;
-}
+  let randomSpecial =['!','@','#','$','%','&'];
 
+  let randomP = randomLetter.concat(randomNumber, randomSpecial);
+
+  let genAnswer = [];
+  
+  if ( userCon === true) {
+       genAnswer.push(randomP.join('').toUpperCase());
+     
+  }
+  if(userCon === true) { 
+      genAnswer.push(randomP.join('').toLowerCase());
+   } else {
+    return;
+  }
+  let news = genAnswer.join('');
+  let makePass = ' ';
+
+  for ( let i = 0; i < convert; i++){
+     
+  makePass += news.charAt(Math.floor(Math.random() * news.length));
+
+}
+  return makePass;
+}
 
 
 // Assignment Code, DO NOT EDIT ANTHING  BELOW THIS LINE
@@ -39,4 +55,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
 
